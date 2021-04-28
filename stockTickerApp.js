f4ba4d2db9a513a329b2bbf2d7d0bf0e62672a72
joyho796 <56969406+joyho796@users.noreply.github.com>
2021-04-29 05:32:35 +0800

    Add files via upload

--- stockTickerApp.js
+++ stockTickerApp.js
@@ -5,8 +5,12 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://joyho796:Z135qet!@cluster0.h00z1.mongodb.net/stock_app?retryWrites=true&w=majority";
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
+
 console.log("loaded");
+
 http.createServer(function (req, res) {
      console.log(req.url);
      if (req.url == "/") {
