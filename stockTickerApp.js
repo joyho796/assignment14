3faca8199b739b5e1a32dad278b61b5d97e6e578
joyho796 <56969406+joyho796@users.noreply.github.com>
2021-04-29 05:39:51 +0800

    Add files via upload

--- stockTickerApp.js
+++ stockTickerApp.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
 });
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 http.createServer(function (req, res) {
      console.log(req.url);
      if (req.url == "/") {
